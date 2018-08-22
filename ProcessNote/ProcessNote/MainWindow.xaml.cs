2357bc3faa1e3ef910465090e158d92643bf9644
gyertya <gyertya@gmail.com>
2018-08-22 14:43:34 +0200

    1st User Story done(sorry fo no commits), process list displayed

---
+++ ProcessNote/ProcessNote/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProcessNote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProcessGrid.ItemsSource = ProcessHandler.LoadProcesses();
+            ProcessGrid.AutoGenerateColumns = true;
+        }
+
+        private void ProcessGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ProcessGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DetailsGrid.ItemsSource = ProcessHandler.LoadProcesses();
+            ProcessGrid.AutoGenerateColumns = true;
+        }
+    }
+}
---
+++ ProcessNote/ProcessNote/ProcessHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessNote
+{
+    class ProcessHandler
+    {
+        public static List<BaseProcess> LoadProcesses()
+        {
+            List<BaseProcess> processes = new List<BaseProcess>();
+            foreach (var item in Process.GetProcesses())
+            {
+                processes.Add(new BaseProcess(item.Id, item.ProcessName));
+            }
+            return processes;
+
+        }
+    }
+}
