32ede2128dc121745d7cca820850fe101ce3d15e
ujfalusizsolt1 <ujfalusi.zsolt1@gmail.com>
2018-08-23 16:08:37 +0200

    Always on top checkbox completed

--- ProcessNote/ProcessNote/MainWindow.xaml.cs
+++ ProcessNote/ProcessNote/MainWindow.xaml.cs
@@ -40,5 +40,14 @@ namespace ProcessNote
             TestBox.Text = ProcessHandler.GetProcessDetails(selectedRow);
         }
 
+        private void AlwaysOnTopCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = true;
+        }
+
+        private void AlwaysOnTopCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = false;
+        }
     }
 }
