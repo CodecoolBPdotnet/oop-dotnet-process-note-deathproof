bca7655ef64a5b53c082daf9a3a10d8abef4211c
ujfalusizsolt1 <ujfalusi.zsolt1@gmail.com>
2019-01-29 13:45:24 +0100

    added new fields to BaseProcess

--- ProcessNote/ProcessNote/BaseProcess.cs
+++ ProcessNote/ProcessNote/BaseProcess.cs
@@ -8,13 +8,34 @@ namespace ProcessNote
 {
     public class BaseProcess
     {
+        public static List<BaseProcess> Processses { get; set; }
         public int PID { get; set; }
         public string Name { get; set; }
+        public string CPUUsage { get; set; }
+        public string MemoryUsage { get; set; }
+        public string RunningTime { get; set; }
+        public DateTime StartTime { get; set; }
 
         public BaseProcess(int pID, string name)
         {
+            if (Processses == null)
+            {
+                Processses = new List<BaseProcess>();
+            }
             this.PID = pID;
             this.Name = name;
+        }
+
+        public static BaseProcess GetBaseProcessByPID(int PID )
+        {
+            foreach (var item in BaseProcess.Processses)
+            {
+                if (item.PID == PID)
+                {
+                    return item;
+                }
+            }
+            throw new NotImplementedException();
         }
     }
 }
